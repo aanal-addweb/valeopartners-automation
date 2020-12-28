37effa571719de461c44f7b3b6168f4712b75669
addweb <aanal.addweb@gmail.com>
2020-12-28 15:41:44 +0530

    valeo script changes

--- src/test/java/com/valeopartnersautmation/TestCase/BaseCase.java
+++ src/test/java/com/valeopartnersautmation/TestCase/BaseCase.java
@@ -4,6 +4,7 @@ import com.valeopartnersautmation.Constants.CommonVars;
 import com.valeopartnersautmation.Reports.ReportClass;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
 
 public class BaseCase extends ReportClass {
@@ -12,10 +13,28 @@ public class BaseCase extends ReportClass {
 
     @BeforeTest
     public void initialize() {
-        System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir") + "chromedriver");
-        driver = new ChromeDriver();
-        CommonVars constantVars = new CommonVars();
-        driver.get(constantVars.url);
+//        System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir") + "chromedriver");
+//        driver = new ChromeDriver();
+//        CommonVars constantVars = new CommonVars();
+//        driver.get(constantVars.url);
+//        driver.manage().window().maximize();
+
+//        headless
+        System.setProperty("Webdriver.chrome.driver", System.getProperty("user.dir") + "chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        // options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("--headless");
+        options.addArguments("window-size=1200,600");
+        options.addArguments("--disable-extensions"); // disdemoabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
+        driver.get("https://valeodev:F0rD3v0nly!@dev.reports.valeopartners.com/");
+//      CommonVar constantVars = new CommonVar();
+//      driver.get("https://valeodev:F0rD3v0nly!@dev.reports.valeopartners.com/");
         driver.manage().window().maximize();
+        System.out.println("Title of the page is -> " + driver.getTitle());
     }
 }
