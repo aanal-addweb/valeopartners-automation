1e5db9086925b5459c099b3f5edce6e11124b97a
aanal-addweb <aanal.addweb@gmail.com>
2021-01-20 13:58:57 +0530

    changed report class path

--- src/test/java/com/valeopartnersautmation/Reports/ReportClass.java
+++ src/test/java/com/valeopartnersautmation/Reports/ReportClass.java
@@ -19,7 +19,8 @@ public class ReportClass {
     @BeforeSuite
     public void initReport() throws SQLException
     {
-        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/Valeo_ScriptExecution_Report.html");
+//        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/Valeo_ScriptExecution_Report.html");
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("/test-output/Valeo_ScriptExecution_Report.html"));
         extent = new ExtentReports ();
         extent.attachReporter(htmlReporter);
         extent.setSystemInfo("Host Name", "Valeo");
